24c54fab31948d19276a3cc04a449df160423a6b
AlxV07 <alexander.kai.chen@gmail.com>
2025-07-02 14:30:17 -0500

    website update

--- script.js
+++ script.js
@@ -1,61 +1,10 @@
-let dynamicTheme = false
-document.getElementById('toggle-theme').onclick = () => {
-    dynamicTheme = !dynamicTheme
-    nextTheme()
-}
-let mantis = '#a5e596'
-let bee = '#f5ea9e'
-let glaucus = '#bccaee'
-let dynamicColors = [bee, mantis, glaucus]
 let images = [getImg(0), getImg(1), getImg(2)]
 let themeIdx = images.length - 2;
 
 function nextTheme() {
     themeIdx = (themeIdx + 1) % images.length
-    transitionColor()
     transitionImage()
 }
-
-function transitionColor() {
-    let t;
-    let u;
-    if (dynamicTheme) {
-        t = 'background-color 4s ease';
-        u = ', color 4s ease';
-    } else {
-        t = 'none';
-        u = '';
-    }
-    document.body.style.transition = t + u;
-    document.querySelectorAll('nav').forEach(function (a) { a.style.transition = t; })
-    document.querySelectorAll('section').forEach(function (a) { a.style.transition = t; })
-    document.querySelectorAll('.blog-post').forEach(function (a) { a.style.transition = t; })
-
-    let targetColor;
-    let targetBackgroundColor;
-    let targetSectionBackgroundColor;
-    let targetBlogPostBackgroundColor;
-    if (dynamicTheme) {
-        document.getElementById('toggle-theme').textContent = 'Disable Dynamic Theme'
-        targetColor = dynamicColors[themeIdx]
-        targetBackgroundColor = '#000000'
-        targetSectionBackgroundColor = '#070707'
-        targetBlogPostBackgroundColor = '#101010'
-    } else {
-        document.getElementById('toggle-theme').textContent = 'Enable Dynamic Theme'
-        targetColor = '#000000'
-        targetBackgroundColor = '#f5f5f5'
-        targetSectionBackgroundColor = '#eaeaea'
-        targetBlogPostBackgroundColor = '#eeeeee'
-    }
-    document.body.style.color = targetColor;
-    document.body.style.backgroundColor = targetBackgroundColor
-    document.querySelectorAll('nav').forEach(function (a) { a.style.backgroundColor = targetSectionBackgroundColor; })
-    document.querySelectorAll('section').forEach(function (a) { a.style.backgroundColor = targetSectionBackgroundColor; })
-    document.querySelectorAll('.blog-post').forEach(function (a) { a.style.backgroundColor = targetBlogPostBackgroundColor; })
-    document.querySelectorAll('a').forEach(function (a) { a.style.color = targetColor; })
-}
-
 async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
